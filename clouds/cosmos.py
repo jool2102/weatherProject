964c87c3c287d60d443796a5c21b65b5dbd07f89
Joakim Hammargren <91683500+jool2102@users.noreply.github.com>
2022-12-13 15:03:10 +0100

    Add files via upload

---
+++ clouds/cosmos.py
@@ -0,0 +1,33 @@
+import requests,json
+from azure.cosmos import CosmosClient
+import time
+i = 1
+while True:
+    i = i+1
+
+    api_key = "02535417df1956f146800cc5c3292f45"
+    openweather_api = "http://api.openweathermap.org/data/2.5/weather?"
+    cityName = "Stockholm"
+
+    complete_url = openweather_api + "appid=" + api_key + "&q=" + cityName
+    response = requests.get(complete_url)
+
+    x = response.json()
+    ## DIN cosmos info
+    client = CosmosClient(
+        "https://tempproject.documents.azure.com:443/",
+
+        "4k1MchPsefWNETiCseMBSJufb1VvgmcGcySD7zOzApnpr6s4OYcLSQjJ3Qv6SBxWnYmraDgnGoKFACDbV9Rljg=="
+    )
+
+    data = x
+    x["id"] = "Api_id" + str(i)
+    print(x)
+    print(type(x))
+
+    database = client.get_database_client("ToDoList")
+    
+    container = database.get_container_client("Items")
+    container.create_item(data)
+    
+    time.sleep(5)
